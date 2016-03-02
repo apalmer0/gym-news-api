e190a8583868519462b8cae821f877a8e406bfbb
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-03-02 13:44:32 -0500

    Remove profiles and all references to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301184656) do
+ActiveRecord::Schema.define(version: 20160302183608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,16 +57,6 @@ ActiveRecord::Schema.define(version: 20160301184656) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string   "first_name", null: false
-    t.string   "last_name",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "token",           null: false
@@ -85,5 +75,4 @@ ActiveRecord::Schema.define(version: 20160301184656) do
   add_foreign_key "climbs", "gyms"
   add_foreign_key "favorites", "climbs"
   add_foreign_key "favorites", "users"
-  add_foreign_key "profiles", "users"
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,20 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe 'associations' do
-    def association
-      described_class.reflect_on_association(:profile)
-      # stands for the class you're working with in this block, specifically 'Article'
-    end
 
-    it 'has one profile' do
-      expect(association).not_to be_nil
-      expect(association.options[:inverse_of]).not_to be_nil
-    end
-
-    it 'deletes associated profile when destroyed' do
-      expect(association.options[:dependent]).to eq(:destroy)
-    end
-
-  end
 end
